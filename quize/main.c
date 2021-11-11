ffd27ed3d00679d62687906e4dc241bf965e502c
hamza-at-youcode <hamzayocude@gmail.com>
2021-11-11 16:16:12 +0100

    Fixe logical problem

--- quize/main.c
+++ quize/main.c
@@ -51,7 +51,7 @@ int main(){
     printf("\n\n\tYour score is: %d - ",score);
 
     if (score > 8 && score <= 12) printf("Good");
-    else if (score > 12 && score < 16) printf("Very Good!");
+    else if (score > 12 && score <= 16) printf("Very Good!");
     else if (score > 16) printf("Exellent!!!");
     else printf("Try again ;(");
     
