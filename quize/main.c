25e257b92e3a60e2a0360c275e1451b247a86bdb
hamza-at-youcode <hamzayocude@gmail.com>
2021-11-11 10:49:46 +0100

    Complete basic version

--- quize/main.c
+++ quize/main.c
@@ -1,5 +1,32 @@
 #include<stdio.h>
 
 int main(){
+
+    int i,j;
+
+    char *qst[] = {"What is my name?","How old am I?","What is my favorite clr?"};
+
+    char *choice[][3] = {
+        {"Hamza","Ayoub","Yassine"},
+        {"22","23","20"},
+        {"Green","B&W","Any"}
+    };
+
+    int ans[] = {0,0,1},tmpAns,score = 0;    
+    
+    for (i = 0; i < 3; i++)
+    {
+        printf("Q%d: %s\n",i,qst[i]);
+        for (j = 0; j < 3; j++)
+        {
+           printf("\t%d - %s\n",j,choice[i][j]);
+        }
+        
+        printf("Answer: ");
+        scanf("%d",&tmpAns);
+        if(ans[i] == tmpAns) score+=2;
+    }
+
+    printf("\n\n\tYour score is: %d",score);
     
 }
